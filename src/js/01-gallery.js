7b02c473b07ff60065e70bdcf6ae5a128b22848f
Viacheslav ALEKSIEIEV <bagetta007@gmail.com>
2023-01-12 14:12:30 +0200

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,8 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+
+// Change code below this line
 console.log(galleryItems);
+
 const galleryRef = document.querySelector('.gallery');
 
 galleryRef.insertAdjacentHTML('afterbegin', greateMarcup(galleryItems));
