cdf2659266c032799c021e85e6850dd637390d87
wrestling007 <bagetta007@gmail.com>
2023-01-12 22:32:32 +0200

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCAL_TIME = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+player.setCurrentTime(localStorage.getItem(LOCAL_TIME));
+
+function onTimeUpdate(data) {
+  localStorage.setItem(LOCAL_TIME, data.seconds);
+}
